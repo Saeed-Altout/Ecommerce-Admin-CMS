12efda5988fbef971edb4505936f2eb262ce6839
Saeed-Altout <saeedaltoutpro@gmail.com>
2025-12-14 07:45:36 +0300

    feat: Integrate Clerk for authentication and establish initial root and auth page structure.

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Auth",
+  description: "Auth",
+};
+
+export default function AuthLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <main className="flex h-screen items-center justify-center">{children}</main>;
+}
