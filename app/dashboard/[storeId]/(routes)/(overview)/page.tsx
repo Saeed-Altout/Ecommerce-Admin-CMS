2563ed2cdee1a8950c12a4a741d056fb3abae174
Saeed-Altout <saeedaltoutpro@gmail.com>
2025-12-21 10:42:34 +0300

    feat: Implement overview page with revenue, sales, and product count cards, along with interactive chart component for enhanced store dashboard experience.

--- app/dashboard/[storeId]/(routes)/(overview)/page.tsx
+++ app/dashboard/[storeId]/(routes)/(overview)/page.tsx
@@ -1,3 +1,66 @@
-export default function StorePage() {
-  return <div>test</div>;
+import { BadgeIcon, DollarSignIcon, Wallet2Icon } from "lucide-react";
+
+import { getSalesCount, getStoreCount, getTotalRevenue } from "@/data/overview";
+
+import {
+  Card,
+  CardAction,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Heading } from "@/components/ui/heading";
+import { ChartAreaInteractive } from "./_components/chart-area-interactive";
+
+export default async function OverviewPage({
+  params,
+}: {
+  params: Promise<{ storeId: string }>;
+}) {
+  const storeId = (await params).storeId;
+  const totalRevenue = await getTotalRevenue(storeId);
+  const salesCount = await getSalesCount(storeId);
+  const storeCount = await getStoreCount(storeId);
+
+  return (
+    <>
+      <Heading title="Dashboard" description="Overview of your store" />
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
+        <Card>
+          <CardHeader>
+            <CardDescription>Total Revenue</CardDescription>
+            <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+              {totalRevenue}$
+            </CardTitle>
+            <CardAction>
+              <DollarSignIcon className="size-4" />
+            </CardAction>
+          </CardHeader>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardDescription>Sales</CardDescription>
+            <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+              +{salesCount}
+            </CardTitle>
+            <CardAction>
+              <Wallet2Icon className="size-4" />
+            </CardAction>
+          </CardHeader>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardDescription>Product in store</CardDescription>
+            <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+              +{storeCount}
+            </CardTitle>
+            <CardAction>
+              <BadgeIcon className="size-4" />
+            </CardAction>
+          </CardHeader>
+        </Card>
+      </div>
+      <ChartAreaInteractive />
+    </>
+  );
 }
